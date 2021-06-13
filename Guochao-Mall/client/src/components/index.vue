d8e936b25cc3d744ca84c5be670066729f3594a5
HSGodHS <67578339+HSGodHS@users.noreply.github.com>
2021-06-13 20:15:19 +0800

    Add files via upload

---
+++ Guochao-Mall/client/src/components/index.vue
@@ -0,0 +1,178 @@
+<template>
+  <div id="app">
+<!--    <el-header></el-header>-->
+    <el-container style="height: 100%; border: 1px solid #eee">
+      <el-aside width="auto" style="background-color: rgb(238, 241, 246)">
+        <div class="app-side-logo">
+          <img src="@/assets/logo.png"
+               :width=auto
+               height="60" />
+        </div>
+
+        <el-menu router
+                 :default-active="$route.path"
+                :collapse="isCollapse"
+><el-button  v-on:click="toggleSideBar"
+             style="margin-left: 0.2rem;
+        margin-top: 0.1rem"><i class="el-icon-menu" ></i>
+           </el-button>
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-s-goods"></i>
+               <span>商品管理</span></template>
+            <el-menu-item-group>
+              <el-menu-item index="/product/item">
+                   <span>商品</span></el-menu-item>
+              <el-menu-item index="/product/cart">
+                   <span>购物车</span></el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-s-order"></i>
+                 <span>订单管理</span></template>
+            <el-menu-item-group>
+              <el-menu-item index="/order/order">
+                <span>订单</span></el-menu-item>
+<!--              <el-menu-item index="/order/item">订单条目</el-menu-item>-->
+<!--              <el-menu-item index="/order/payment">支付信息</el-menu-item>-->
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title"><i class="el-icon-s-custom"></i>
+              <span>用户管理</span></template>
+            <el-menu-item-group>
+              <el-menu-item index="/user/baseinfo">
+                <span>修改密码</span></el-menu-item>
+              <el-menu-item index="/user/userinfo">
+                <span>个人资料</span></el-menu-item>
+              <el-menu-item index="/user/usermanage">
+                <span>用户管理</span></el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-container>
+      <el-header>
+        <el-row>
+        <div align="right">
+          <el-dropdown trigger="hover"
+                       :hide-on-click="false">
+            <span class="el-dropdown-link">
+              {{ username }}
+              <span> ,欢迎来到国潮电商系统！</span>
+              <i class="el-icon-arrow-down el-icon--right"></i>
+            </span>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item @click.native="gotoUserinfo">设置</el-dropdown-item>
+              <el-dropdown-item divided
+                                @click.native="logout">退出登录</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+        <div align="left">
+          <el-button type="primary" size="mini" icon="el-icon-arrow-left" @click="prev">返回</el-button>
+        </div>
+        </el-row>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+      </el-container>
+  </el-container>
+</div>
+</template>
+<style type="text/css">
+/*
+	找到html标签、body标签，和挂载的标签
+	都给他们统一设置样式
+*/
+  html,body,#app,.el-container{
+        /*设置内部填充为0，几个布局元素之间没有间距*/
+        padding: 0px;
+         /*外部间距也是如此设置*/
+        margin: 0px;
+        /*统一设置高度为100%*/
+        height: 100%;
+    }
+
+</style>
+
+<script>
+export default {
+  name: 'index',
+
+  data() {
+    return {
+      username: '',
+      isCollapse: false,
+      screenWidth: document.body.clientWidth, // 屏幕宽度
+      timer: false,
+    }
+  },
+  methods: {
+    toggleSideBar() {
+      this.isCollapse = !this.isCollapse;
+    },
+    logout: function () {
+      this.$confirm('确认退出?', '提示', {})
+        .then(() => {
+          sessionStorage.removeItem('user');
+          this.$router.push('/auth/login');
+        })
+        .catch(() => { });
+    },
+    gotoUserinfo() {
+      this.$router.push({
+        path:'/user/userinfo',
+        name: 'userinfo',
+      })
+    },
+
+
+    prev() {
+      this.$router.go(-1);
+    }
+  },
+  mounted: function () {
+    let user = sessionStorage.getItem('user');
+    if (user) {
+      this.username = user;
+    }
+
+    // 监听窗口大小
+    window.onresize = () => {
+      return (() => {
+        this.screenWidth = document.body.clientWidth
+      })()
+    }
+
+  },
+  computed: {
+    isCollapse: {
+      get () {
+        return this.screenWidth < 768
+      },
+      set () {
+      }
+    }
+  },
+  watch: {
+    screenWidth (val) {
+      if (!this.timer) {
+        this.screenWidth = val
+        if (this.screenWidth < 768) {
+          this.isCollapse = true
+        }
+        if (this.screenWidth > 768) {
+          this.isCollapse = false
+        }
+        this.timer = true
+        let that = this
+        setTimeout(function () {
+          that.timer = false
+        }, 400)
+      }
+    }
+  },
+}
+</script>
+
---
+++ Guochao-Mall/database/mall_system_order_item.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `mall_system` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mall_system`;
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mall_system
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `order_item`
+--
+
+DROP TABLE IF EXISTS `order_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_item` (
+  `item_id` int NOT NULL AUTO_INCREMENT,
+  `order_no` varchar(50) NOT NULL,
+  `unit_price` decimal(10,2) NOT NULL,
+  `quantity` int NOT NULL,
+  `total_price` decimal(10,2) DEFAULT NULL,
+  `product_id` int DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `FK_product_item2` (`order_no`),
+  KEY `FK_product_item` (`product_id`),
+  CONSTRAINT `FK_product_item` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_product_item2` FOREIGN KEY (`order_no`) REFERENCES `mall_order` (`order_no`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_item`
+--
+
+LOCK TABLES `order_item` WRITE;
+/*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
+INSERT INTO `order_item` VALUES (1,'1491753014256',199.00,1,199.00,2);
+/*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-05-07 14:10:26
